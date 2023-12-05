8df040ebf753bc9a6d0acec6c912ca94e1ae6b52
Quan Liu <liuquan2017@sjtu.edu.cn>
2023-12-05 14:50:27 +0800

    Update test_kitti.py

--- scripts/test_kitti.py
+++ scripts/test_kitti.py
@@ -109,13 +109,10 @@ def main(config):
   success_meter, rte_meter, rre_meter = AverageMeter(), AverageMeter(), AverageMeter()
   data_timer, feat_timer, reg_timer = Timer(), Timer(), Timer()
 
-  # test_loader.dataset.__getitem__(610)
-
   test_iter = test_loader.__iter__()
   N = len(test_iter)
   n_gpu_failures = 0
 
-  # downsample_voxel_size = 2 * config.voxel_size
   dists_success = []
   dists_fail = []
   dists_nn = []
@@ -125,27 +122,13 @@ def main(config):
   T_gt = []
   T_est = []
 
-  # rte_thresh = 0.6
-  # rre_thresh = 1.5
-
   rte_thresh = config.rte_thresh
   rre_thresh = config.rre_thresh
   print(f"rre thresh: {rre_thresh}; rte_thresh: {rte_thresh}")
 
-  # for i in range(100):
   for i in range(len(test_iter)):
     data_timer.tic()
-    # for _ in range(207):
-    #   data_dict = test_iter.next() 
-    #   print(f"fast loaded {i}'th data", flush=True)
-    #   i = i+1
     data_dict = test_iter.next()
-    # try:
-    #   data_dict = test_iter.next()
-    # except ValueError:
-    #   n_gpu_failures += 1
-    #   logging.info(f"# Erroneous GPU Pair {n_gpu_failures}")
-    #   continue
     data_timer.toc()
     xyz0, xyz1 = data_dict['pcd0'][0], data_dict['pcd1'][0]
     T_gth = data_dict['T_gt']
@@ -153,9 +136,6 @@ def main(config):
     dist_gth = np.sqrt(np.sum((T_gth[:3, 3].cpu().numpy())**2))
     trans_gt.append(dist_gth)
     xyz0np, xyz1np = xyz0.numpy(), xyz1.numpy()
-
-    # pcd0 = make_open3d_point_cloud(xyz0np)
-    # pcd1 = make_open3d_point_cloud(xyz1np)
 
     # torch.cuda.synchronize()
     with torch.no_grad():
@@ -172,36 +152,7 @@ def main(config):
       feat_timer.toc()
 
     xyz0_corr, xyz1_corr = find_corr(xyz0, xyz1, F0, F1, subsample_size=5000)
-    # if i==207:
-    #   np.savez(f'corr_fcgf', src=xyz0.cpu().detach().numpy(), tgt=xyz1.cpu().detach().numpy(),
-    #           corr_src=xyz0_corr, corr_tgt=xyz1_corr, 
-    #           T_gt=T_gth.cpu().detach().numpy())
-    # raise ValueError
     dists_nn.append(evaluate_nn_dist(xyz0_corr, xyz1_corr, T_gth))
-    
-    # i=0
-    # for enc, pcd in zip([enc0, enc1], [pcd0, pcd1]):
-    #   _, batch_enc_feats = enc.detach().decomposed_coordinates_and_features
-    #   # print(np.size(batch_enc_feats))
-    #   from sklearn.manifold import TSNE
-    #   tsne = TSNE(n_components=3)
-    #   print('doing tsne')
-    #   X_embedded = tsne.fit_transform(batch_enc_feats[0].cpu().numpy())
-    #   print('finished')
-    #   xyz = np.asarray(pcd.points)
-    #   inds = np.where(xyz[:, 2] < 0)
-    #   bag = np.hstack((xyz, X_embedded))
-    #   bag = np.delete(bag, inds, 0)
-    #   xyz = bag[:, :3]
-    #   X_embedded = bag[:, 3:]
-    #   for dim in range(3):
-    #     max_value = np.max(X_embedded[:, dim])
-    #     min_value = np.min(X_embedded[:, dim])
-    #     X_embedded[:, dim] = (X_embedded[:, dim] - min_value) / (max_value - min_value)
-    #   np.save(f'points{i}.npy', np.asarray(pcd.points))
-    #   np.save(f'colors{i}.npy', np.asarray(pcd.colors))
-    #   i+=1
-    # raise ValueError
 
     n_points = 5000
     ########################################
@@ -226,14 +177,6 @@ def main(config):
           ], o3d.pipelines.registration.RANSACConvergenceCriteria(4000000, 10000))
       T_ransac = torch.from_numpy(ransac_result.transformation.astype(np.float32))
     else:
-      # result_ransac = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
-      #     pcd0, pcd1, feat0, feat1, False, distance_threshold,
-      #     o3d.pipelines.registration.TransformationEstimationPointToPoint(False), 4, [
-      #         o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
-      #         o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)
-      #     ],o3d.pipelines.registration.RANSACConvergenceCriteria(50000, 1000))
-      # T_ransac = torch.from_numpy(result_ransac.transformation.astype(np.float32))
-
       xyz0, xyz1 = torch.from_numpy(xyz0np).to(F0.device), torch.from_numpy(xyz1np).to(F0.device)
       T_ransac, _, _, _ = matcher.estimator(xyz0[None,:], xyz1[None,:], F0[None,:], F1[None,:])
       T_ransac = T_ransac[0].to("cpu")
@@ -248,10 +191,6 @@ def main(config):
     trace_matrix[[0,1,2], [0,1,2]] = torch.min(torch.ones(3), trace_matrix[[0,1,2], [0,1,2]])
     rre = np.arccos((np.trace(trace_matrix) - 1) / 2)
 
-    # Check if the ransac was successful. successful if rte < 2m and rre < 5◦
-    # http://openaccess.thecvf.com/content_ECCV_2018/papers/Zi_Jian_Yew_3DFeat-Net_Weakly_Supervised_ECCV_2018_paper.pdf
-    # rte_thresh = 0.6
-    # rre_thresh = 1.5
     
     if rte < rte_thresh:
       rte_meter.update(rte)
@@ -282,34 +221,6 @@ def main(config):
 
   print(f"rre thresh: {rre_thresh}; rte_thresh: {rte_thresh}")
 
-  # np.savez(f"results_fcgf_LoNuScenes.npz", rres=list_rre, rtes=list_rte, dists=trans_gt, T_gt=T_gt, T_est=T_est)
-
-  # dists_nn = np.array(dists_nn, dtype=object) # dists_nn: N_pairs * N_num_point_one_frame
-  # hit_ratios = []
-  # for seed_tao_1 in range(1, 11):
-  #   tao_1 = seed_tao_1 * 0.1
-  #   hit_ratios.append(np.mean((dists_nn < tao_1).astype(float), axis=1).tolist())
-
-  # fmrs = []
-  # # hit_ratios: 10 * N_pairs
-  # for seed_tao_2 in range(1, 11):
-  #   tao_2 = seed_tao_2 * 0.01
-  #   fmrs.append(np.mean((np.array(hit_ratios) > tao_2).astype(float), axis=1).tolist())
-  
-  # fmrs: 10*10, dim0: seed_tao_2; dim2: seed_tao_1
-  # np.save('FMRs_fcgf.npy', fmrs)
-
-  # if config.use_old_pose:
-  #   np.savez(f"results_fcgf_{config.pair_max_dist}.npz", rres=list_rre, rtes=list_rte, dists=trans_gt, T_gt=T_gt, T_est=T_est)
-  # else:
-  # np.savez(f"results_fcgf_nuScenes_{config.pair_max_dist}.npz", rres=list_rre, rtes=list_rte, dists=trans_gt, T_gt=T_gt, T_est=T_est)
-
-  # np.save("success_dists.npy", dists_success)
-  # np.save("fail_dists.npy", dists_fail)
-
-  # np.save("list_rte_fcgf.npy", list_rte)
-  # np.save("list_rre_fcgf.npy", list_rre)
-
   logging.info(
       f"RTE: {rte_meter.avg}, var: {rte_meter.var}," +
       f" RRE: {rre_meter.avg}, var: {rre_meter.var}, Success: {success_meter.sum} " +
@@ -326,7 +237,6 @@ if __name__ == '__main__':
   parser.add_argument('--test_phase', default='test', type=str)
   parser.add_argument('--LoKITTI', default=False, type=str2bool)
   parser.add_argument('--LoNUSCENES', default=False, type=str2bool)
-  parser.add_argument('--DAIR_V2X', default=False, type=str2bool)
   parser.add_argument('--test_num_thread', default=5, type=int)
   parser.add_argument('--pair_min_dist', default=None, type=int)
   parser.add_argument('--pair_max_dist', default=None, type=int)
@@ -352,10 +262,6 @@ if __name__ == '__main__':
   config.phase = 'test'
   config.use_RANSAC = args.use_RANSAC
   # config.debug_force_icp_recalculation = True
-
-  if args.DAIR_V2X:
-    config.dataset = "VICRegistrationDataset"
-    config.test_phase = "val"
 
   if args.LoNUSCENES:
     config.dataset = "PairComplementNuscenesDataset"
